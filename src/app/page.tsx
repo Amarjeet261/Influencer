52dfd431679448be74b2dc33fb0ce1ddc537cb0f
Amarjeet261 <amarjeetrajput326@gmail.com>
2025-06-15 22:48:12 +0530

    work start

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/component/Footer/Footer";
+import Header from "@/component/Header/Header";
+import Navbar from "@/component/Navbar/Navbar";
+
+
+export default function Home() {
+  return (
+   <section className="">
+    <Navbar/>
+    <Header/>
+    <Footer/>
+   </section>
+  );
+}
